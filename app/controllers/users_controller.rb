924f16e1374e80f8da10299c16c114a201e1b6a0
yusuke-hub <yk1210140408@gmail.com>
2019-12-24 00:24:07 +0000

    fixed routeing home#top

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def index
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
+    @user = current_user
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
   end
   def edit
