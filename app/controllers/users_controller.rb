97de10f578993c4c8d166d3f7b89313b6346f2d8
yusuke-hub <yk1210140408@gmail.com>
2019-12-24 00:00:02 +0000

    add end in users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def index
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
-
+  end
   def edit
   	@user = User.find(params[:id])
   end
@@ -36,4 +36,4 @@ class UsersController < ApplicationController
   	end
    end
 
-end
+endx
